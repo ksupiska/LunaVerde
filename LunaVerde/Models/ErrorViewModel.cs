e0b425279e2a3da52112af9d93a8cb53a8fc15c9
Ksusha <https://github.com/ksupiska>
2024-11-24 09:54:04 +0300

    Initial commit

---
+++ LunaVerde/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LunaVerde.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
