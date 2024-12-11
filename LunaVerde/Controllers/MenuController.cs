5f4f5c799e058336efbd61e88329995ad83c3163
Ksusha <https://github.com/ksupiska>
2024-12-11 14:14:42 +0300

    Adding new pages + outputting data to pages from the database on the site

--- LunaVerde/Controllers/MenuController.cs
+++ LunaVerde/Controllers/MenuController.cs
@@ -1,17 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using LunaVerde.Data;
 
 namespace LunaVerde.Controllers
 {
     public class MenuController : Controller
     {
+        private readonly LunaVerdeDBContext _context;
+        public MenuController(LunaVerdeDBContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Breakfast()
+        public async Task<IActionResult> Breakfast()
         {
-            return View();
+            // Получение данных из базы данных
+            // Получение списка блюд из базы данных
+            var breakfastItems = _context.Menu.Where(m => m.MenuItem == "Breakfast").ToList();
+
+            return View(breakfastItems);
+        }
+
+        public async Task<IActionResult> Lunch()
+        {
+            // Получение данных из базы данных
+            // Получение списка блюд из базы данных
+            var lunchItems = _context.Menu.Where(m => m.MenuItem == "Lunch").ToList();
+            return View(lunchItems);
+        }
+        public async Task<IActionResult> Dinner()
+        {
+            // Получение данных из базы данных
+            // Получение списка блюд из базы данных
+            var dinnerItems = _context.Menu.Where(m => m.MenuItem == "Dinner").ToList();
+            return View(dinnerItems);
+        }
+        public async Task<IActionResult> Drink()
+        {
+            // Получение данных из базы данных
+            // Получение списка блюд из базы данных
+            var drinkItems = _context.Menu.Where(m => m.MenuItem == "Drink").ToList();
+            return View(drinkItems);
         }
     }
 }
--- LunaVerde/Models/Class.cs
+++ LunaVerde/Models/Class.cs
@@ -7,6 +7,7 @@
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string ImagePath { get; set; }
+        public string MenuItem { get; set; }
     }
 
     public class Order
