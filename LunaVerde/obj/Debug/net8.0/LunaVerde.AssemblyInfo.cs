4974bd9f9ef5bfc8de07239d164cac13d0c25b6b
Ksusha <https://github.com/ksupiska>
2024-12-13 12:28:17 +0300

    Minor changes to pages

--- LunaVerde/obj/Debug/net8.0/LunaVerde.AssemblyInfo.cs
+++ LunaVerde/obj/Debug/net8.0/LunaVerde.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LunaVerde")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64aa4ddd9659285204376213a87dc7b26aef4e28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b632fcfa29b10346e9a30828cba28c6e983be71")]
 [assembly: System.Reflection.AssemblyProductAttribute("LunaVerde")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LunaVerde")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
